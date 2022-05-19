9e0fe3d641272902a3adcb86acc3aa0fcc3e8895
EliasLindvall <70134851+EliasLindvall@users.noreply.github.com>
2022-05-19 09:36:07 +0200

    finished product

--- GBGolfbollar_project/javascript/shopping_cart.js
+++ GBGolfbollar_project/javascript/shopping_cart.js
@@ -12,21 +12,41 @@ function ready() {
         var button = trashCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
     }
-    var quantityInputs = getElementsByClassName('form-control')
-    for (var i = o; i < quantityInputs.lentgh; i++) {
-        input = quantityInputs[i]
+    var quantityInputs = document.getElementsByClassName('form-control')
+    console.log(quantityInputs)
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
 
-    var addToCartButtons = getElementsByClassName('cart-add')
+    var addToCartButtons = document.getElementsByClassName('cart-add')
+    console.log(addToCartButtons)
     for (var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
 
+    var selectClassForms = document.getElementsByClassName('form-select')
+    for (var i = 0; i < selectClassForms.length; i++) {
+        var form = selectClassForms[i]
+        form.addEventListener('change', classChanged)
+    }
+
+    document.getElementsByClassName('purchase-button')[0].addEventListener('click', purchaseClicked)
+    
+
 }
 
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+console.log(roundToTwo(1.6004))
 
+
+function purchaseClicked() {
+    alert('Tack för ditt köp!')
+    document.location.reload(true)
+}
 
 function removeCartItem(event) {
     var buttonClicked = event.target
@@ -43,16 +63,79 @@ function quantityChanged(event) {
     updateCartTotal()       
 }
 
+function classChanged(event) {
+    var form = event.target
+    var formValue = form.value
+    var productContent = form.parentElement
+    var priceElement = productContent.getElementsByClassName('original-price')[0]
+    var price = parseFloat(priceElement.innerText)
+    var finalPrice
+    console.log(price)
+    console.log(formValue)
+    
+    if (formValue == 1) {
+        finalPrice = (price*2)
+    }
+    else if (formValue == 2) {
+        finalPrice = (price*1.5)
+    }
+    else if (formValue == 3) {
+        finalPrice = (price*1)
+    }
+    else if (formValue == 4) {
+        finalPrice = (price*0.75)
+    }
+    console.log(finalPrice)
+
+    productContent.getElementsByClassName('price')[0].innerText = `${Math.round(finalPrice * 100) / 100}kr`
+}
+
 function addToCartClicked(event){
     var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('title')[0].innerText
-    console.log(title)
+    var shopItem = button.parentElement.parentElement.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('product-name')[0].innerText
+    var price = shopItem.getElementsByClassName('price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('img-fluid')[0].src
+    var classForm = shopItem.getElementsByClassName('form-select')[0]
+    var classElement = classForm.options[classForm.selectedIndex].text;
+    console.log(title, price, imageSrc, classElement)
+    addItemToCart(title, price, imageSrc, classElement)
+    updateCartTotal()
+}
+
+
+function addItemToCart(title, price, imageSrc, classElement){
+    var cartRow = document.createElement('tr')
+    cartRow.classList.add('cartRow')
+    var cartItems = document.getElementById('cart-container')
+    var cartItemTitles = document.getElementsByClassName('item-title')
+    console.log(cartItemTitles.innerText)
+    for (var i = 0; i < cartItemTitles.length; i++){
+        if (cartItemTitles[i].innerText == title){
+            alert('This item is already added to the cart!')
+            return
+        }
+    }
+    var cartRowContents = `
+    <td><img src="${imageSrc}" class="img-cart"></td>
+    <td><strong class="item-title">${title}</strong><p>${classElement}</p></td>
+    <td>
+      <input class="form-control" type="number" value="1">
+    </td>
+    <td class="price">${price}</td>
+    <td>
+      <button type="button" class="btn btn-danger fa fa-trash-o" rel="tooltip"></a>
+    </td>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('fa-trash-o')[0].addEventListener('click', removeCartItem)
+    ready()
+    
 }
 
 function updateCartTotal() {
     var cartItemContainer = document.getElementById('cart-container')[0]
-    var cartRows = document.getElementsByName('cartRow')
+    var cartRows = document.getElementsByClassName('cartRow')
     var total = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
@@ -67,5 +150,6 @@ function updateCartTotal() {
         total = total + (price * quantity)
         
     }
+    total = total.toFixed(2)
     document.getElementsByClassName('cart-total')[0].innerText = total + 'kr'
-}
+} 
